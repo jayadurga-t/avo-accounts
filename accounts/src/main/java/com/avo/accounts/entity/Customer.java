5c65df4ac13b17731e4801a8bd428e1cf0dd8087
Jaya Durga Tirunelveli <jayadurga.tirunelveli@accell-it-services.com>
2026-01-31 15:01:19 +0530

    accounts: add JPA entities and repositories

---
+++ accounts/src/main/java/com/avo/accounts/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.avo.accounts.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
---
+++ accounts/src/main/java/com/avo/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.avo.accounts.repository;
+
+import com.avo.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
